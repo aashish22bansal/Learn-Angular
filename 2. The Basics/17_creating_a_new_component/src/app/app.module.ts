f1d0266b57e9cdb0edcc83a93cc8b7d38a6cdd97
Aashish Bansal <aashish22bansal@gmail.com>
2023-03-23 00:58:17 +0530

    Using Custom Components Bug Fixed
    Section 2: The Basics -> 19
    The content from the app.component.html file was present in the server.component.html which is wrong because the Component must be called for use from the Parent/Root Component.

--- 2. The Basics/17_creating_a_new_component/src/app/app.module.ts
+++ 2. The Basics/17_creating_a_new_component/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-// import { HttpModule } from '@angular/http';
+import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http'
-// import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,7 +29,7 @@ import { ServerComponent } from './server/server.component';
     BrowserModule, // This module gives us all the base functionality using which we will need to start our application.
     AppRoutingModule,
     FormsModule,
-    // HttpModule,
+    HttpModule,
     HttpClientModule,
     // HttpClient
   ],
