cd2eb059524d83f0f6c5f8c62db1846efe0b70a9
Aashish Bansal <aashish22bansal@gmail.com>
2023-03-23 01:41:03 +0530

    Working with Component Templates Completed
    Section 2: The Basics -> 21 Completed
    In-lining Templates

--- 2. The Basics/17_creating_a_new_component/src/app/servers/servers.component.ts
+++ 2. The Basics/17_creating_a_new_component/src/app/servers/servers.component.ts
@@ -3,6 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
+  /** We can write the HTML Code with the template file as: */
+  // template: '
+  //   <app-server>  WRITE HTML CODE WITHIN THIS SPACE.  </app-server>
+  //   <app-server>  WRITE HTML CODE WITHIN THIS SPACE.  </app-server>
+  // ',
+  /** We can write the HTML Code within this Space if the code happens to be either simple or short otherwise it is advisable that the code should be written in an external file and the file must be mapped to this location. In allows Inlining the Templates. We can omit a Selector but we cannot omit a Template. */
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
