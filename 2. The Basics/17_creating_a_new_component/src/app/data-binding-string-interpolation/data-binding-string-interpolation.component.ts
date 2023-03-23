4549b79fece3c4006a17a70a5cc9db1bdaeea5c9
Aashish Bansal <aashish22bansal@gmail.com>
2023-03-23 23:37:02 +0530

    String Interpolation Completed
    Section 2: The Basics -> 26 Completed
    String Interpolation with the help of data variables and member functions.

--- 2. The Basics/17_creating_a_new_component/src/app/data-binding-string-interpolation/data-binding-string-interpolation.component.ts
+++ 2. The Basics/17_creating_a_new_component/src/app/data-binding-string-interpolation/data-binding-string-interpolation.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./data-binding-string-interpolation.component.css']
 })
 export class DataBindingStringInterpolationComponent {
+  serverId: number = 10;
+  serverStatus: string = "offline";
+  /** To fetch this data, we can use Data Binding. */
 
+  getServerStatus(){
+    return this.serverStatus;
+  }
+  /** This method can be called in the same way we use String Interpolation. */
 }
