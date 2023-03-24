f4387c9eaa7a057b8e2e2c8a5af65e013ec46ce1
Aashish Bansal <aashish22bansal@gmail.com>
2023-03-24 07:10:25 +0530

    Getting the Index when using ngFor
    Section 2: The Basics -> 43 Completed
    Using Date() to obtain the Timestamp and using multiple elements in a single ngFor Loop.

--- 2. The Basics/17_creating_a_new_component/src/app/assignment-three-practicing-directives/assignment-three-practicing-directives.component.ts
+++ 2. The Basics/17_creating_a_new_component/src/app/assignment-three-practicing-directives/assignment-three-practicing-directives.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssignmentThreePracticingDirectivesComponent implements OnInit {
   showSecret = false;
-  log: number[] = [];
+  log: Date[] = [];
   constructor(){}
   ngOnInit(): void {
       
@@ -15,6 +15,6 @@ export class AssignmentThreePracticingDirectivesComponent implements OnInit {
 
   onToggleDetails(){
     this.showSecret = !this.showSecret;
-    this.log.push(this.log.length + 1);
+    this.log.push(new Date());
   }
 }
