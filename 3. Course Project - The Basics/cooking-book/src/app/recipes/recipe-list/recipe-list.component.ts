154d9a0b5dbffd8eaa4e379f1b079fabe6f2ce86
Aashish Bansal <aashish22bansal@gmail.com>
2023-03-25 19:09:25 +0530

    Outputting a List of Recipes with ngFor Completed
    Section 3: Course Project - The Basics -> 54 Completed
    Adding Data to the Object and accessing object data through the Component

--- 3. Course Project - The Basics/cooking-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ 3. Course Project - The Basics/cooking-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit{
   // Adding an Array of Recipes
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe','This is a Test Recipe','https://iwashyoudry.com/wp-content/uploads/2021/01/American-Goulash-Recipe.jpg'),
+    new Recipe('A Test Recipe','This is a Test Recipe','https://iwashyoudry.com/wp-content/uploads/2021/01/American-Goulash-Recipe.jpg'),
+    new Recipe('A','B','C')
+  ];
   constructor(){}
 
   ngOnInit(){}
