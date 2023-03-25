4ac206f6c1d6b522a442e4aadf85aa10ef584dbb
Aashish Bansal <aashish22bansal@gmail.com>
2023-03-26 01:29:11 +0530

    Splitting Apps into Components Completed
    Section 3: Course Project: The Basics -> 64 Completed
    Created the application, generated the components and then modified the code as per need.

---
+++ 5. Components & Databinding Deep Dive/databinding/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // title = 'databinding';
+  // type: string = '';
+  serverElements = [];
+  newServerName: string = '';
+  newServerContent: string = '';
+
+  
+}
