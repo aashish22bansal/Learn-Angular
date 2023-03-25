318ffd37f91cd18f1a87216444ff67b4bdb8eb53
Aashish Bansal <aashish22bansal@gmail.com>
2023-03-25 23:39:11 +0530

    Creating and Outputting the Shopping List Completed
    Section 3: Course Project: The Basics -> 58 Completed
    Added objects to the "ingredients" list and fixed bug in the UI shopping-list.component.html file.

--- 3. Course Project - The Basics/cooking-book/src/app/shopping-list/shopping-list.component.ts
+++ 3. Course Project - The Basics/cooking-book/src/app/shopping-list/shopping-list.component.ts
@@ -9,9 +9,9 @@ import { Ingredient } from '../shared/ingredient.model';
 export class ShoppingListComponent implements OnInit {
   // Creating the Array of ingredients
   ingredients: Ingredient[] = [
-    new Ingredient('Apples', 10);
-    new Ingredient('Banana', 20);
-    new Ingredient('Cucumber', 30);
+    new Ingredient("Apples", 10),
+    new Ingredient('Banana', 20),
+    new Ingredient('Cucumber', 30)
   ];
   constructor(){}
   ngOnInit(){}
