6bca5d51c88017b52db2cbb3879105914d8c3b7b
Aashish Bansal <aashish22bansal@gmail.com>
2023-03-31 00:57:54 +0530

    Allowing the User to Add Ingredients to the Shopping List Completed
    Section 6: Course Project - Components & Databinding -> 89 Completed
    Added Local References to shopping-edit.component.ts, then created the references to pass data to the shopping-list.component.html in the form of an Ingredient Object by creating and emitting an event.
    Further, event listener was created in shopping-list.component.html to listen to the passed data and then pass that data to a method to add it to the array which will be used to read data to display on the screen.

--- 6. Course Project - Components & Databinding/cooking-book/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ 6. Course Project - Components & Databinding/cooking-book/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent {
+export class ShoppingEditComponent implements OnInit {
+  @ViewChild('namedInput') namedInputReference: ElementRef;
+  @ViewChild('amountInput') amountInputReference: ElementRef;
+  // Emitting the new Event to pass this data to the parent component which manages the array of ingredients.
+  
+  @Output() ingredientAdded = new EventEmitter<Ingredient>(); //This is just a Type Definition with the Ingredient Model.
+  // We will need to listen to this event in the shopping-list.component.html
 
+  constructor(){
+  }
+
+  ngOnInit() {
+      
+  }
+
+  onAddItem(){
+    // creating the new Item
+    const newIngredient = new Ingredient(this.namedInputReference.nativeElement.value, this.amountInputReference.nativeElement.value);
+    // We need this to be constant because we are not going to change these values and we do not want them to be changed in transmission.
+
+    // Emitting the event
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
